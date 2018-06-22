27392d5ee410ff9a572b01558cdb09bdf0890fe8
jgyun <jgyun@kr.ibm.com>
2018-06-22 14:52:01 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var SwaggerExpress = require('swagger-express-mw');
+var app = require('express')();
+module.exports = app; // for testing
+
+var config = {
+  appRoot: __dirname, // required config
+  swaggerSecurityHandlers: {
+    api_key: function (req, authOrSecDef, scopesOrApiKey, cb) {
+      // your security code
+      console.log('scopesOrApiKey:',scopesOrApiKey)
+      if ('1234' === scopesOrApiKey) {
+        cb();
+      } else {
+        cb(new Error('access denied!'));
+      }
+    }
+  }
+};
+
+SwaggerExpress.create(config, function(err, swaggerExpress) {
+  if (err) { throw err; }
+
+  // install middleware
+  swaggerExpress.register(app);
+
+  var port = process.env.PORT || 10010;
+  app.listen(port);
+
+  if (swaggerExpress.runner.swagger.paths['/hello']) {
+    console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
+  }
+});
+
+var SwaggerUi = require('swagger-tools/middleware/swagger-ui');
+
+SwaggerExpress.create(config, function(err, swaggerExpress) {
+
+  // add swagger-ui (/docs)
+  app.use(SwaggerUi(swaggerExpress.runner.swagger));
+
+  // install middleware
+  swaggerExpress.register(app);
+
+});
